5f44d378e2b0b0acd39952d6c489251886712fee
Hugo Aboud <hugoaboud@gmail.com>
2023-03-23 05:35:41 -0300

    filter out database exceptions

--- src/App/ExceptionHandler.ts
+++ src/App/ExceptionHandler.ts
@@ -2,6 +2,7 @@ import Logger from '@ioc:Adonis/Core/Logger'
 import HttpExceptionHandler from '@ioc:Adonis/Core/HttpExceptionHandler'
 import { HttpContextContract } from "@ioc:Adonis/Core/HttpContext";
 import { Exception } from '@adonisjs/core/build/standalone';
+import DBException from '../Exception/DBException';
 
 /**
     Formats exceptions thrown by the controllers.
@@ -13,20 +14,28 @@ export default class ExceptionHandler extends HttpExceptionHandler {
 
   public async handle(error: Exception, ctx: HttpContextContract) {
    
-      console.error(error)
+    console.error(error)
 
-      let messages:any[] = [];
-      const e_messages = (error as any).messages
+    let messages:any[] = [];
+    const e_messages = (error as any).messages
+    if (error instanceof DBException) {
+      messages = [
+        'Database error.'
+      ]
+    }
+    else {
       if (e_messages) {
-          Object.values(e_messages).forEach(msgs => {
-              messages.push(...(msgs as any));
-          })
+        Object.values(e_messages).forEach(msgs => {
+          messages.push(...(msgs as any));
+        })
       }
       else {
-          messages = [error.message.replace(error.code+': ','')]
+        messages = [error.message.replace(error.code+': ','')]
       }
+    }
 
-    return ctx.response.status(error.status).send({
+    const status = error.status || 500
+    return ctx.response.status(status).send({
       errors: {
         code: error.code,
         base: messages
